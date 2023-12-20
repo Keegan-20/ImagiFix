7ba9cfa5a738caef5956f31821696b7566a84c6f
Keegan <keegancolaco20@gmail.com>
2023-12-20 21:27:44 +0530

    PWA

--- js/service worker.js
+++ js/service worker.js
@@ -3,7 +3,7 @@ self.addEventListener("install",e =>{
     //Installation of PWA
         console.log("Instalation done !!"); 
         
-        //caching of data
+        //caching of the  data
         e.waitUntil(
             caches.open("static").then(cache =>{
                 //files to cached when there is slow or no internet
@@ -11,4 +11,14 @@ self.addEventListener("install",e =>{
                 "./img/logo-192x192.png","./js/script.js"]);
             })
         );
+    });
+    
+    self.addEventListener("fetch",e =>{
+        e.respondeWith(
+             caches.match(e.request)
+             .then(response =>{
+                   return response  || fetch(e.request); // to check if data is availble in cache than respond from cache db storage otherwise go to fetch (make a network call to get the data)
+              })
+        )
+    //    console.log(`Intercepting fetch request for : ${e.request.url}`);
     });
